fc3f3b1bc0bf687cb19ee984ebacaf57755c9cce
Stevenbenavidesj@gmail.com <147666314+StevenBenavides@users.noreply.github.com>
2026-06-16 12:01:10 -0500

    Segundo

--- actividad_01/institucion/models.py
+++ actividad_01/institucion/models.py
@@ -14,10 +14,23 @@ class Museo (models.Model):
         return self.nombre
 
 class Guia_museo (models.Model):
-    nombre_completo = models.CharField(max_length=100)
-    anios_experiencia_guia = models.CharField(max_length=100)
-    idiomas_hablados = models.ForeignKey(Estudiante, on_delete=models.CASCADE, \
-            related_name="numeros_telefonicos")
-
+    nombre_completo = models.CharField(max_length=200)
+    anios_experiencia_guia = models.IntegerField()
+    idiomas_hablados =  models.CharField(max_length=200)
+    
+    museo = models.ForeignKey(Museo, on_delete=models.CASCADE, \
+            related_name="guias")
     def __str__(self):
-        return "%s %s" % (self.telefono, self.tipo)
+        return self.nombre_completo
+
+class Exposicion (models.Model):
+    titulo_exhibicion = models.CharField(max_length=200)
+    duracion_meses = models.IntegerField()
+    costo_produccion = models.DecimalField(max_digits=100, decimal_places=2)
+    tematica = models.CharField(max_length=200)
+
+    museo = models.ForeignKey(Guia_museo, on_delete=models.SET_NULL, null=True, \
+            related_name="exposiciones")
+    def __str__(self):
+        return self.titulo_exhibicion
+
