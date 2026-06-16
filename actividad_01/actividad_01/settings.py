e0602df2ec6f581b391b7f971d740ef43bedb2c1
Stevenbenavidesj@gmail.com <147666314+StevenBenavides@users.noreply.github.com>
2026-06-16 16:39:57 -0500

    Final

--- actividad_01/actividad_01/settings.py
+++ actividad_01/actividad_01/settings.py
@@ -74,9 +74,17 @@ WSGI_APPLICATION = 'actividad_01.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_postgres',
+        'USER': 'steven',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
     }
 }
 
